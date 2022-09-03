5a6daef8ea72701bf13d2fa95bbd1833b7d77726
Gerald Clark <geraldclarkaudio@gmail.com>
2022-09-03 01:09:17 -0500

    set up some colliders for the floor and walls. Added ui

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private bool canOpen;
+
+    private StarterAssetsInputs _inputs;
+
+    private Animator _anim;
+    private UIManager _uiManager;
+    [SerializeField]
+    private GameObject instructionPanel;
+
+    private void Start()
+    {
+        _inputs = GameObject.Find("PlayerCapsule").GetComponent<StarterAssetsInputs>();
+        _anim = GetComponentInChildren<Animator>();
+        _uiManager = GameObject.Find("UI").GetComponent<UIManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canOpen = true;
+            _uiManager.OpenDoorText();
+            instructionPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canOpen = false;
+            instructionPanel.SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        if (canOpen == false)
+        {
+       
+            return;
+        }
+
+        else
+        {
+    
+
+            if (_inputs.interact == true)
+            {
+                Debug.Log("oPENED");
+                _anim.SetTrigger("Open");
+                canOpen = false;
+               
+            }              
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text text;
+
+    public void OpenDoorText()
+    {
+        text.text = "Press E To Open The Door";
+    }
+
+    public void Speak()
+    {
+        text.text = "Press E To Speak";
+    }
+
+}
